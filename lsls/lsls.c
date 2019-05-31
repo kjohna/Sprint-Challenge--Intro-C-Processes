f3028470175e6494c6c3357401b0b457b62cb24a
kjohna <43828763+kjohna@users.noreply.github.com>
2019-05-31 09:38:14 -0600

    parsing input, opening directories

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,13 +6,41 @@
  */
 int main(int argc, char **argv)
 {
-  // Parse command line
+    // default to current directory
+    char *dir_to_print = "./";
+    // store directory stream pointer
+    DIR *dir;
+    // Parse command line
+    if (argc == 2)
+    {
+        dir_to_print = argv[1];
+        printf("attempt to print directory: %s\n", dir_to_print);
+    }
+    else if (argc > 2)
+    {
+        printf("More than one arg passed in, taking first only.\n");
+        dir_to_print = argv[1];
+        printf("attempt to print directory: %s\n", dir_to_print);
+    }
+    else
+    {
+        printf("Print current directory: %s\n", dir_to_print);
+    }
 
-  // Open directory
+    // Open directory
+    dir = opendir(dir_to_print);
+    if (dir != NULL)
+    {
+        // Repeatly read and print entries
+        // while(){};
+        printf("directory '%s' opened successfully.\n", dir_to_print);
+        // Close directory
+        closedir(dir);
+    }
+    else
+    {
+        printf("error opening directory: %s\n", dir_to_print);
+    }
 
-  // Repeatly read and print entries
-
-  // Close directory
-
-  return 0;
+    return 0;
 }
